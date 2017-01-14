fa5a16bb6bd1f8e1b804e110027b2f53a7f005d1
Daniel Smith <jellymann@gmail.com>
2017-01-14 19:13:48 +0200

    refactor: games channel name into method

--- app/channels/games_channel.rb
+++ app/channels/games_channel.rb
@@ -1,17 +1,23 @@
 class GamesChannel < ApplicationCable::Channel
   def subscribed
     stream_from "games_#{params['game_id']}_channel"
-    ActionCable.server.broadcast "games_#{params['game_id']}_channel",
+    ActionCable.server.broadcast channel_name,
                                  message: "#{current_user.name} joined the game"
   end
 
   def unsubscribed
-    ActionCable.server.broadcast "games_#{params['game_id']}_channel",
+    ActionCable.server.broadcast channel_name,
                                  message: "#{current_user.name} left the game"
   end
 
   def do_a_thing(data)
-    ActionCable.server.broadcast "games_#{data['game_id']}_channel",
+    ActionCable.server.broadcast channel_name,
                                  message: "#{current_user.name} did a thing"
+  end
+
+  private
+
+  def channel_name
+    "games_#{params['game_id']}_channel"
   end
 end
