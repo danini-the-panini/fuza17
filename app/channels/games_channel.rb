461ce5c1abadb514d513650469dbcf03dbf48918
Daniel Smith <jellymann@gmail.com>
2017-01-15 18:58:26 +0200

    spawn players randomly

--- app/channels/games_channel.rb
+++ app/channels/games_channel.rb
@@ -7,9 +7,8 @@ class GamesChannel < ApplicationCable::Channel
 
     time = Time.now
     intial_state = {
-      # x: (rand * 200.0) - 100.0,
-      # y: (rand * 200.0) - 100.0
-      x: 0.0, y: 0.0
+      x: (rand * 200.0) - 100.0,
+      y: (rand * 200.0) - 100.0
     }
 
     ActionCable.server.broadcast channel_name,
