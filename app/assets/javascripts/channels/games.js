3a2211f8f18eb42a7b1ebf4a448e6f464699d0d6
Daniel Smith <jellymann@gmail.com>
2017-01-15 09:25:19 +0200

    threejs

--- app/assets/javascripts/channels/games.js
+++ app/assets/javascripts/channels/games.js
@@ -1,34 +1,63 @@
 const App = require('../cable');
+const THREE = require('three');
 
 $(document).on('turbolinks:load', () => {
   if (App.game) {
     App.game.unsubscribe();
   }
 
-  const things = $('#things');
+  const canvas = $('#game-canvas');
 
-  if (things.length === 0) return;
+  if (canvas.length === 0) return;
 
-  const gameId = things.data('game-id');
+  const gameId = canvas.data('game-id');
+
   App.game = App.cable.subscriptions.create({
     channel: 'GamesChannel',
-    game_id: things.data('game-id')
+    game_id: gameId
   }, {
     connected() {},
 
     disconnected() {},
 
     received(data) {
-      things.append(`<li>${data.message}</li>`)
+      // things.append(`<li>${data.message}</li>`)
     },
 
-    doAThing(game_id) {
-      return this.perform('do_a_thing', { game_id });
+    doAThing() {
+      return this.perform('do_a_thing', { gameId });
     }
   });
 
-  $('#does-a-thing').on('click', e => {
-    App.game.doAThing(things.data('game-id'))
-    e.preventDefault();
+	const scene = new THREE.Scene();
+	const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+	const renderer = new THREE.WebGLRenderer({
+    canvas: canvas.get(0)
   });
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+	const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+	const cube = new THREE.Mesh( geometry, material );
+	scene.add( cube );
+
+	camera.position.z = 5;
+
+  $(window).on('resize', () => {
+    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix();
+  	renderer.setSize( window.innerWidth, window.innerHeight );
+  });
+
+	function render() {
+		requestAnimationFrame( render );
+
+		cube.rotation.x += 0.1;
+		cube.rotation.y += 0.1;
+
+		renderer.render(scene, camera);
+	};
+
+	render();
 });
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def page_class
+    if current_page?(game_path)
+      'page-game-show'
+    end
+  end
 end
